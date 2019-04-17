5656fcb9a04a6db4cbf5a1e846bd08757873162e
Chloe Moon <ymoon@wellesley.edu>
2019-04-17 03:14:42 +0000

    importing packages for app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug import secure_filename
+app = Flask(__name__)
 
+import sys,os,random
+import functions
+
+app.secret_key = 'asdflsgflawiewurhe'
