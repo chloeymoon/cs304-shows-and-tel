3ca48f2ed51f030f79c087367ea034179ae67edc
Chloe Moon <ymoon@wellesley.edu>
2019-05-16 22:00:09 +0000

    attempted to do multiple search (commented out)

--- app.py
+++ app.py
@@ -134,6 +134,11 @@ def search():
         tag_name = request.form['tags'] #.getlist('tags')
         tag_val = request.form['tag-arg'] #.getlist('tag-arg')
 
+        if (title=='' and network=='' and creator=='' and contentwarning==''
+                      and tag_name=='' and tag_val=='' and genre==''):
+            flash("Search using at least one criteria")
+            return redirect(request.referrer)
+            
         if title:
             shows = functions.getResultsByTitle(conn,title)
         if network:
@@ -142,16 +147,36 @@ def search():
             shows = functions.getResultsByCreator(conn,creator)
         if genre:
             shows = functions.getResultsByGenre(conn,genre)
-        if (title=='' and network=='' and creator=='' and contentwarning==''
-                      and tag_name=='' and tag_val=='' and genre==''):
-            flash("Search using at least one criteria")
-            return redirect(request.referrer)
         if tag_name and tag_val:
             shows = functions.getResultsByTags(conn, tag_name, tag_val)
         if contentwarning:
             shows = functions.getResultsByContentWarning(conn,contentwarning)
+            
         return render_template('results.html', shows=shows)
 
+
+# @app.route('/search/', methods=['POST'])
+# def search():
+#     '''Displays all the user requested search results'''
+#     if request.method == 'POST':
+#         conn = functions.getConn('final_project')
+#         title = request.form['title']
+#         network = request.form['network']
+#         creator = request.form['creator']
+#         genre = request.form['genre']
+#         warning = request.form['contentwarning']
+#         tag_name = request.form['tags'] #.getlist('tags')
+#         tag_val = request.form['tag-arg'] #.getlist('tag-arg')
+
+#         if (title=='' and network=='' and creator=='' and warning==''
+#                       and tag_name=='' and tag_val=='' and genre==''):
+#             flash("Search using at least one criteria")
+#             return redirect(request.referrer)
+        
+#         shows = functions.multiplesearch(conn, title, network, creator, genre, warning, tag_name, tag_val)
+#         # print shows
+#         return render_template('results.html', shows=shows)
+        
 if __name__ == '__main__':
     app.debug = True
-    app.run('0.0.0.0',8082)
+    app.run('0.0.0.0',8081)
